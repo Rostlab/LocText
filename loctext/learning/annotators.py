6882830d70e0480a9fcd431f00387dcad69dfb32
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-28 12:58:15 +0200

    reorder for clarity

--- loctext/learning/annotators.py
+++ loctext/learning/annotators.py
@@ -31,6 +31,35 @@ class LocTextBaselineRelationExtractor(RelationExtractor):
 
 class LocTextRelationExtractor(RelationExtractor):
 
+    def __init__(
+            self,
+            entity1_class,
+            entity2_class,
+            rel_type,
+            bin_model,
+            pipeline=None,
+            svmlight=None,
+            execute_pipeline=True):
+
+        super().__init__(entity1_class, entity2_class, rel_type)
+        self.bin_model = bin_model
+        self.svmlight = svmlight if svmlight else SVMLightTreeKernels(model_path=self.bin_model, use_tree_kernel=False)
+        feature_generators = LocTextRelationExtractor.default_feature_generators(self.entity1_class, self.entity2_class)
+        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type, feature_generators=feature_generators)
+        self.execute_pipeline = execute_pipeline
+
+
+    def annotate(self, corpus):
+        if self.execute_pipeline:
+            self.pipeline.execute(corpus, train=False)
+
+        instancesfile = self.svmlight.create_input_file(corpus, 'predict', self.pipeline.feature_set)
+        predictionsfile = self.svmlight.tag(instancesfile)
+        self.svmlight.read_predictions(corpus, predictionsfile, threshold=0)
+
+        return corpus
+
+
     @staticmethod
     def default_feature_generators(prot_e_id, loc_e_id, graphs=None):
 
@@ -122,32 +151,3 @@ class LocTextRelationExtractor(RelationExtractor):
                 prefix=108
             )
         ]
-
-
-    def __init__(
-            self,
-            entity1_class,
-            entity2_class,
-            rel_type,
-            bin_model,
-            pipeline=None,
-            svmlight=None,
-            execute_pipeline=True):
-
-        super().__init__(entity1_class, entity2_class, rel_type)
-        self.bin_model = bin_model
-        self.svmlight = svmlight if svmlight else SVMLightTreeKernels(model_path=self.bin_model, use_tree_kernel=False)
-        feature_generators = LocTextRelationExtractor.default_feature_generators(self.entity1_class, self.entity2_class)
-        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type, feature_generators=feature_generators)
-        self.execute_pipeline = execute_pipeline
-
-
-    def annotate(self, corpus):
-        if self.execute_pipeline:
-            self.pipeline.execute(corpus, train=False)
-
-        instancesfile = self.svmlight.create_input_file(corpus, 'predict', self.pipeline.feature_set)
-        predictionsfile = self.svmlight.tag(instancesfile)
-        self.svmlight.read_predictions(corpus, predictionsfile, threshold=0)
-
-        return corpus
