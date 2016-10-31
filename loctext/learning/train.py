64e81f9e9999e3ab7760848eead6a0aae10ff802
Juan Miguel Cejuela <i@juanmi.rocks>
2016-10-31 15:59:13 +0100

    revamp train & test

--- loctext/learning/train.py
+++ loctext/learning/train.py
@@ -1,26 +1,28 @@
 from loctext.util import PRO_ID, LOC_ID, REL_PRO_LOC_ID, repo_path
 from nalaf.structures.relation_pipelines import RelationExtractionPipeline
-from nalaf.learning.svmlight import SVMLightTreeKernels
+from nalaf.learning.svmlight import SVMLightTreeKernel
 from loctext.learning.annotators import LocTextRelationExtractor
+from nalaf.learning.evaluators import DocumentLevelRelationEvaluator, Evaluations
+from nalaf import print_verbose, print_debug
 
-def parse_arguments(argv):
+def parse_arguments(argv=[]):
     import argparse
 
     parser = argparse.ArgumentParser(description='dooh')
 
     parser.add_argument('--corpus', default="LocText", choices=["LocText"])
-    parser.add_argument('--use_tk', default=False, action='store_true')
+    parser.add_argument('--minority_class', typ=int, default=1, choices=[-1, 1])
+    parser.add_argument('--minority_undersampling', typ=float, default=1, help='e.g. 1 == no undersampling; 0.5 == 50% undersampling')
+    parser.add_argument('--svm_hyperparameter_c', typ=float, default=0.0005)
     parser.add_argument('--use_test_set', default=False, action='store_true')
     parser.add_argument('--k_num_folds', type=int, default=5)
+    parser.add_argument('--use_tk', default=False, action='store_true')
 
     return parser.parse_args(argv)
 
 
-def train_with_argv(argv):
-    args = parse_arguments(argv)
-    corpus = read_corpus(args.corpus)
-
-    return train(corpus, args)
+def parse_arguments_string(arguments=""):
+    return parse_arguments(arguments.split("\s+"))
 
 
 def train(training_set, args):
@@ -31,12 +33,29 @@ def train(training_set, args):
 
     # Learn
     pipeline.execute(training_set, train=True)
-    svmlight = SVMLightTreeKernels(use_tree_kernel=False)  # Beware: should use args, but conflict of Namespace vs object
-    instancesfile = svmlight.create_input_file(training_set, 'train', pipeline.feature_set, minority_class=1, undersampling=0.1)
+    svmlight = SVMLightTreeKernel(use_tree_kernel=args.use_tk)
+    instancesfile = svmlight.create_input_file(training_set, 'train', pipeline.feature_set, minority_class=1) # , minority_class=None, undersampling=0.1)
     svmlight.learn(instancesfile, c=0.0005)
 
     # Alert: we should read the class ids from the corpus
     return LocTextRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID, svmlight.model_path, pipeline=pipeline, svmlight=svmlight)
+
+
+def evaluate(corpus, args):
+    annotator_fun = (lambda training_set: train(training_set, args))
+    evaluator = DocumentLevelRelationEvaluator(rel_type=REL_PRO_LOC_ID, match_case=False)
+
+    evaluations = Evaluations.cross_validate(annotator_fun, corpus, evaluator, args.k_num_folds, use_validation_set=args.use_validation_set)
+    rel_evaluation = evaluations(REL_PRO_LOC_ID).compute(strictness="exact")
+
+    return rel_evaluation
+
+
+def evaluate_with_argv(argv=[]):
+    args = parse_arguments(argv)
+    corpus = read_corpus(args.corpus)
+
+    return evaluate(corpus, args)
 
 
 def read_corpus(corpus_name):
@@ -67,6 +86,39 @@ def read_corpus(corpus_name):
     return corpus
 
 
+def print_corpus_stats(corpus):
+    from nalaf.preprocessing.edges import SimpleEdgeGenerator
+    from nalaf.preprocessing.spliters import NLTKSplitter
+    from nalaf.preprocessing.tokenizers import TmVarTokenizer
+
+    splitter = NLTKSplitter()
+    tokenizer = TmVarTokenizer()
+    edger = SimpleEdgeGenerator(PRO_ID, LOC_ID, REL_PRO_LOC_ID)
+
+    splitter.split(corpus)
+    tokenizer.tokenize(corpus)
+    edger.generate(corpus)
+    corpus.label_edges()
+
+    P = 0
+    N = 0
+
+    for e in corpus.edges():
+        assert e.target != 0, str(e)
+        print_verbose(e, e.target)
+
+        if e.target > 0:
+            P += 1
+        else:
+            N += 1
+
+    # with all (abstract+fulltext), P=614 vs N=1480
+    # with only abstracts -- Corpus size: 100 -- #P=351 vs. #N=308
+    print("Corpus size: {} -- #P={} vs. #N={}".format(len(corpus), P, N))
+
+    return (P, N)
+
+
 if __name__ == "__main__":
     import sys
-    train(sys.argv[1:])
+    evaluate_with_argv(sys.argv[1:])
--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -7,7 +7,7 @@ except SystemError: # Parent module '' not loaded, cannot perform relative impor
 from loctext.util import PRO_ID, LOC_ID, REL_PRO_LOC_ID
 from nalaf.learning.evaluators import DocumentLevelRelationEvaluator, Evaluations
 from loctext.learning.annotators import LocTextBaselineRelationExtractor
-from loctext.learning.train import read_corpus, train
+from loctext.learning.train import read_corpus, train, parse_arguments, evaluate_with_argv
 from nalaf import print_verbose, print_debug
 import math
 import sys
@@ -39,34 +39,6 @@ def test_baseline():
 def test_LocText(use_full_corpus):
     corpus = read_corpus("LocText")
 
-    from nalaf.preprocessing.edges import SimpleEdgeGenerator
-    from nalaf.preprocessing.spliters import NLTKSplitter
-    from nalaf.preprocessing.tokenizers import TmVarTokenizer
-
-    splitter = NLTKSplitter()
-    tokenizer = TmVarTokenizer()
-    edger = SimpleEdgeGenerator(PRO_ID, LOC_ID, REL_PRO_LOC_ID)
-
-    splitter.split(corpus)
-    tokenizer.tokenize(corpus)
-    edger.generate(corpus)
-    corpus.label_edges()
-
-    P = 0
-    N = 0
-
-    for e in corpus.edges():
-        assert e.target != 0, str(e)
-        print(e, e.target)
-        if e.target > 0:
-            P += 1
-        else:
-            N += 1
-
-    # with all (abstract+fulltext), P=614 vs N=1480
-    # with only abstracts -- Corpus size: 100 -- #P=351 vs. #N=308
-    print("Corpus size: {} -- #P={} vs. #N={}".format(len(corpus), P, N))
-
     if (use_full_corpus):
         # 0.1
         # 0.4 0.618421052631579
@@ -79,11 +51,7 @@ def test_LocText(use_full_corpus):
         EXPECTED_F = 0.4906
         EXPECTED_F_SE = 0.0083
 
-    annotator_fun = (lambda train_set: train(train_set, {'use_tk': False}))
-    evaluator = DocumentLevelRelationEvaluator(rel_type=REL_PRO_LOC_ID, match_case=False)
-
-    evaluations = Evaluations.cross_validate(annotator_fun, corpus, evaluator, k_num_folds, use_validation_set=use_validation_set)
-    rel_evaluation = evaluations(REL_PRO_LOC_ID).compute(strictness="exact")
+    rel_evaluation = evaluate_with_argv()
 
     assert math.isclose(rel_evaluation.f_measure, EXPECTED_F, abs_tol=EXPECTED_F_SE * 1.1)
 
