ff29303ad94a4db4fcf812d7ff2f55852614624f
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-03 12:12:20 +0100

    we are so fucked

--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -7,28 +7,22 @@ except SystemError: # Parent module '' not loaded, cannot perform relative impor
 from loctext.util import PRO_ID, LOC_ID, REL_PRO_LOC_ID
 from nalaf.learning.evaluators import DocumentLevelRelationEvaluator, Evaluations
 from nalaf.learning.taggers import StubSameSentenceRelationExtractor
-from loctext.learning.train import read_corpus, train, parse_arguments, evaluate_with_argv
+from loctext.learning.train import read_corpus, evaluate_with_argv
 from nalaf import print_verbose, print_debug
 import math
 import sys
 
-k_num_folds = 5
-use_validation_set = True
 
 def test_baseline():
     corpus = read_corpus("LocText")
-    # Absolute full: abstracts + full text
-    # EXPECTED_F = 0.4234297812279464
-    # EXPECTED_F_SE = 0.0024623653397242064
 
-    # Full: abstracts
-    EXPECTED_F = 0.4547
-    EXPECTED_F_SE = 0.0026
+    EXPECTED_F = 0.6157
+    EXPECTED_F_SE = 0.0028
 
     annotator_fun = (lambda _: StubSameSentenceRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID).annotate)
     evaluator = DocumentLevelRelationEvaluator(rel_type=REL_PRO_LOC_ID, match_case=False)
 
-    evaluations = Evaluations.cross_validate(annotator_fun, corpus, evaluator, k_num_folds, use_validation_set=use_validation_set)
+    evaluations = Evaluations.cross_validate(annotator_fun, corpus, evaluator, k_num_folds=5, use_validation_set=True)
     rel_evaluation = evaluations(REL_PRO_LOC_ID).compute(strictness="exact")
 
     assert math.isclose(rel_evaluation.f_measure, EXPECTED_F, abs_tol=EXPECTED_F_SE * 1.1), rel_evaluation.f_measure
@@ -40,10 +34,6 @@ def test_LocText(use_full_corpus):
     corpus = read_corpus("LocText")
 
     if (use_full_corpus):
-        # 0.1
-        # 0.4 0.618421052631579
-        # 0.5
-        # full 0.5807962529274006
         EXPECTED_F = 0.5433
         EXPECTED_F_SE = 0.0028
     else:
@@ -60,4 +50,4 @@ def test_LocText(use_full_corpus):
 
 if __name__ == "__main__":
     test_baseline()
-    test_LocText(use_full_corpus=('--use-full-corpus' in sys.argv))
+    test_LocText(use_full_corpus=('--corpus_percentage 1' in sys.argv))
