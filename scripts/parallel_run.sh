3929c97871b89fb03d35e99d0790a35bd0ad85cc
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-09 17:24:32 +0100

    implement

--- scripts/parallel_run.sh
+++ scripts/parallel_run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+id=`date +%s%N`
+
+# Sort by F-Measure: tail -n 1 logs/*.log | sort -k 5 -t " "
+
 for minority_class in `seq -1 2 +1`; do
-  for majority_class_undersampling in `seq 0.05 0.05 1`; do
-    for c in None `seq 0.0005 0.0005 0.1000`; do # the None is intentional and means svm_learn default
+  for c in None `seq 0.0005 0.0005 0.1000`; do # the None is intentional and means svm_learn default
+    for majority_class_undersampling in `seq 0.05 0.05 1`; do
       #time echo $c 2>&1 | tee "loctext_${minority_class}_${majority_class_undersampling}_${c}.log"
-      time python loctext/learning/train.py --corpus_percentage 1.0 --minority_class $minority_class --majority_class_undersampling $majority_class_undersampling --svm_hyperparameter_c $c 2>&1 | tee "loctext_m${minority_class}_u${majority_class_undersampling}_c${c}.log"
+      #time python loctext/learning/train.py --corpus_percentage 1.0 --minority_class $minority_class --majority_class_undersampling $majority_class_undersampling --svm_hyperparameter_c $c 2>&1 | tee logs/"loctext_id${id}_m${minority_class}_u${majority_class_undersampling}_c${c}.log" &
+      time python loctext/learning/train.py --corpus_percentage 1.0 --minority_class $minority_class --majority_class_undersampling $majority_class_undersampling --svm_hyperparameter_c $c &> logs/"loctext_id${id}_m${minority_class}_u${majority_class_undersampling}_c${c}.log" &
     done
+    wait
   done
 done
