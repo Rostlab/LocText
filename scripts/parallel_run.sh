1e72ee21fd96385c9d5bb902fb384eb0fb06a4eb
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-10 11:47:37 +0100

    rename scripts

--- scripts/parallel_run.sh
+++ scripts/parallel_run.sh
@@ -5,7 +5,7 @@ logsdir="$logsdir/$id"
 mkdir -p $logsdir
 
 # Sort by F-Measure:
-#  * oldIFS=$IFS; IFS=$'\n'; time for f in `tail -n 1 $logsdir/*.log | grep -oP "(?<=f_measure=).*" | sort -b -k 1,1 -t " " -h | tail -n 1000`; do grep -o "f_measure=$f" $logsdir/*.log; done | tee $logsdir/loctext_1000_best_runs.log; IFS=$oldIFS
+#  * oldIFS=$IFS; IFS=$'\n'; time for f in `tail -n 1 $logsdir/*.log | grep -oP "(?<=f_measure=).*" | sort -b -k 1,1 -t " " -h | tail -n 1000`; do grep -o "f_measure=$f" $logsdir/*.log; done | tee $logsdir/loctext_best_1000_runs_id${id}.log; IFS=$oldIFS
 
 for minority_class in `seq -1 2 +1`; do
   for c in None `seq 0.0005 0.0005 0.1000`; do # the None is intentional and means svm_learn default
