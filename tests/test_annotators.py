5d4d270114eed3a658268214b272557401f23504
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-14 19:20:08 +0100

    recompute with fixed evaluator

--- tests/test_annotators.py
+++ tests/test_annotators.py
@@ -21,9 +21,9 @@ TEST_MIN_CORPUS_PERCENTAGE = 0.4
 def test_SS_baseline():
     corpus = read_corpus("LocText")
 
-    # Baseline Computation(precision=0.6077586206896551, precision_SE=0.002781236497769688, recall=0.6238938053097345, recall_SE=0.004052746798458239, f_measure=0.6157205240174672, f_measure_SE=0.002826453358117873)
+    # Computation(precision=0.6083150984682714, precision_SE=0.002974704942625582, recall=0.6233183856502242, recall_SE=0.004130201948613626, f_measure=0.6157253599114065, f_measure_SE=0.0030062001054202924)
     EXPECTED_F = 0.6157
-    EXPECTED_F_SE = 0.0028
+    EXPECTED_F_SE = 0.0030
 
     annotator_gen_fun = (lambda _: StubSameSentenceRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID).annotate)
     evaluator = DocumentLevelRelationEvaluator(rel_type=REL_PRO_LOC_ID)
@@ -60,9 +60,9 @@ def test_LocText_SS(corpus_percentage):
         EXPECTED_F = 0.6178
         EXPECTED_F_SE = 0.0027
     else:
-        # Computation(precision=0.7426470588235294, precision_SE=0.004447039958950779, recall=0.6121212121212121, recall_SE=0.005972946581336089, f_measure=0.6710963455149502, f_measure_SE=0.0043836182031360155)
-        EXPECTED_F = 0.6711
-        EXPECTED_F_SE = 0.0043
+        # Computation(precision=0.753731343283582, precision_SE=0.004064280002070562, recall=0.6158536585365854, recall_SE=0.006147889829984894, f_measure=0.6778523489932885, f_measure_SE=0.004461978434634661)
+        EXPECTED_F = 0.6779
+        EXPECTED_F_SE = 0.0045
 
     _test_LocText(corpus_percentage, model='SS', EXPECTED_F=EXPECTED_F)
 
@@ -70,9 +70,9 @@ def test_LocText_SS(corpus_percentage):
 def test_DS_baseline():
     corpus = read_corpus("LocText")
 
-    # Computation(precision=0.4149560117302053, precision_SE=0.002457395067634639, recall=0.6206140350877193, recall_SE=0.0037411116863785665, f_measure=0.49736379613356774, f_measure_SE=0.00233567117305811)
-    EXPECTED_F = 0.4974
-    EXPECTED_F_SE = 0.0023
+    # Computation(precision=0.4196969696969697, precision_SE=0.0024518002206926812, recall=0.6210762331838565, recall_SE=0.0038995731275083797, f_measure=0.5009041591320074, f_measure_SE=0.002367560805132471)
+    EXPECTED_F = 0.5009
+    EXPECTED_F_SE = 0.0024
 
     edge_generator = SentenceDistanceEdgeGenerator(PRO_ID, LOC_ID, REL_PRO_LOC_ID, distance=1)
 
@@ -95,8 +95,8 @@ def test_LocText_DS(corpus_percentage):
         EXPECTED_F = 0.4094
         EXPECTED_F_SE = 0.0024
     else:
-        # Computation(precision=0.5254237288135594, precision_SE=0.004919604548886615, recall=0.3712574850299401, recall_SE=0.00506665114212087, f_measure=0.43508771929824563, f_measure_SE=0.004266620575873462)
-        EXPECTED_F = 0.4351
+        # Computation(precision=0.5217391304347826, precision_SE=0.004661534209213446, recall=0.36585365853658536, recall_SE=0.00518652082659298, f_measure=0.43010752688172044, f_measure_SE=0.004278857080197886)
+        EXPECTED_F = 0.4301
         EXPECTED_F_SE = 0.0043
 
     _test_LocText(corpus_percentage, model='DS', EXPECTED_F=EXPECTED_F)
@@ -110,8 +110,8 @@ def test_LocText_Combined(corpus_percentage):
         EXPECTED_F = 0.5734
         EXPECTED_F_SE = 0.0024
     else:
-        # Computation(precision=0.5844748858447488, precision_SE=0.0032793659586925978, recall=0.7710843373493976, recall_SE=0.004115510298431316, f_measure=0.6649350649350649, f_measure_SE=0.002684996682841366)
-        EXPECTED_F = 0.6649
+        # Computation(precision=0.5887850467289719, precision_SE=0.0033947585471817603, recall=0.7682926829268293, recall_SE=0.004128234454709121, f_measure=0.6666666666666666, f_measure_SE=0.0027263757211551205)
+        EXPECTED_F = 0.6667
         EXPECTED_F_SE = 0.0027
 
     _test_LocText(corpus_percentage, model='Combined', EXPECTED_F=EXPECTED_F)
