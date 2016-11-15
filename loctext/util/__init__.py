ccfed5c6806563f2ec161d19d4cce205d989a2cd
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-15 15:30:41 +0100

    add ids

--- loctext/util/__init__.py
+++ loctext/util/__init__.py
@@ -2,8 +2,12 @@ import os
 
 PRO_ID = 'e_1'
 LOC_ID = 'e_2'
-# ORG_ID = 'e_3'
+ORG_ID = 'e_3'
 REL_PRO_LOC_ID = 'r_5'
+
+UNIPROT_NORM_ID = 'n_7'
+GO_NORM_ID = 'n_8'
+TAXONOMY_NORM_ID = 'n_9'
 
 repo_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
