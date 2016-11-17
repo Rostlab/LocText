08828ee29e08fcc90f2daa6a909fb22b0f8c581a
Juan Miguel Cejuela <i@juanmi.rocks>
2016-11-17 16:10:26 +0100

    start basic implementation of absolute equals

--- loctext/learning/evaluations.py
+++ loctext/learning/evaluations.py
@@ -7,4 +7,17 @@ GO_TREE = simple_parse_GO.simple_parse(repo_path(["resources", "ontologies", "go
 
 
 def relation_equals_uniprot_go(gold, pred):
+    # Note: the | separator is defined by and depends on nalaf
+
+    if gold == pred:
+        return True
+
+    [_, g_pro_key, g_n_7, g_loc_key, g_n_8] = gold.split("|")
+    assert g_pro_key == UNIPROT_NORM_ID
+    assert g_loc_key == GO_NORM_ID
+
+    [_, p_pro_key, p_n_7, p_loc_key, p_n_8] = pred.split("|")
+    assert p_pro_key == UNIPROT_NORM_ID
+    assert p_loc_key == GO_NORM_ID
+
     return False
