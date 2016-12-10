d69f965f81fe6ee0b7c3192f9708cff4051ea1d9
shpendm <shpendmahmuti@gmail.com>
2016-12-10 11:08:09 +0100

    Simple testing with testStringTagger.txt file

--- tests/test_String_tagger.py
+++ tests/test_String_tagger.py
@@ -1,11 +1,15 @@
 from nalaf.utils.readers import StringReader
 from loctext.learning.annotators import StringTagger
+import os
 
 def get_string_tagger_post_request_response():
 
-    string_tagger_request_text = ""
-
-    dataset = StringReader(string_tagger_request_text).read()
+    #string_tagger_request_text = "Arginine metabolism in Saccharomyces cerevisiae: subcellular localization of the enzymes.\nSubcellular localization of enzymes of arginine metabolism in Sacchar"  #    {"entities":[{"end": 4,"normalizations":[{"id":"ENSP00000269305","type":9606},{"id":"P04637|P53_HUMAN","type":"uniprot:9606"}],"start":1}]}
+    filename = 'stringTaggerData.txt'
+    with open(os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/resources/corpora/LocText/'+filename, 'r') as content_file:
+        content = content_file.read()
+    dataset = StringReader(content).read()
+    #dataset = StringReader(string_tagger_request_text).read()
 
     # Verify entity offsets - No warnings should be displayed
     dataset.validate_entity_offsets()
@@ -13,11 +17,5 @@ def get_string_tagger_post_request_response():
     return dataset
 
 def test_string_tagger():
-    dataset = get_string_tagger_post_request_response()
-
+    dataset= get_string_tagger_post_request_response()
     StringTagger().annotate(dataset)
-
-    # assert statements needs to be written here for verification based on test.
-
-
-
