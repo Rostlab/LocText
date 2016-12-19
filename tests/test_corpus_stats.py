45feb79e3e68c1620dda31451e55f6abaf275cc3
Juan Miguel Cejuela <i@juanmi.rocks>
2016-12-19 16:42:32 +0100

    consider non-explicitly added relations too and refactor

--- tests/test_corpus_stats.py
+++ tests/test_corpus_stats.py
@@ -6,7 +6,7 @@ except SystemError:  # Parent module '' not loaded, cannot perform relative impo
 
 from loctext.util import PRO_ID, LOC_ID, REL_PRO_LOC_ID
 from nalaf.learning.evaluators import DocumentLevelRelationEvaluator, Evaluations
-from nalaf.learning.taggers import StubSameSentenceRelationExtractor, StubRelationExtractor
+from nalaf.learning.taggers import StubSamePartRelationExtractor, StubRelationExtractor
 from loctext.learning.train import read_corpus, evaluate_with_argv
 from nalaf import print_verbose, print_debug
 from nalaf.preprocessing.edges import SentenceDistanceEdgeGenerator
@@ -20,122 +20,72 @@ from loctext.learning.evaluations import relation_accept_uniprot_go, GO_TREE
 from nalaf.features import get_spacy_nlp_english
 
 
-SENTENCE_SPLITTER = NLTKSplitter()
-TOKENIZER = NLTK_TOKENIZER  # GenericTokenizer(lambda string: (tok.text for tok in nlp.tokenizer(string)))
-# nlp = get_spacy_nlp_english(load_parser=False)
-# TOKENIZER = GenericTokenizer(lambda string: (tok.text for tok in nlp.tokenizer(string)))
-
-
 CORPUS_PERCENTANGE = 1.0
 
 
 def test_count_relations_dists_with_repetitions():
-    ENTITY_MAP_FUN = Entity.__repr__
-    RELATION_ACCEPT_FUN = None  # meaning: str.__eq__
-
-    # Documents 100
-    nums = Counter({'D0': 351, 'D1': 95, 'D2': 53, 'D3': 23, 'D5': 9, 'D6': 8, 'D4': 7, 'D7': 2, 'D9': 2})
-    nums_sum = sum(nums.values())
-    percts = Counter({'D0': 0.6381818181818182, 'D1': 0.17272727272727273, 'D2': 0.09636363636363636, 'D3': 0.04181818181818182, 'D5': 0.016363636363636365, 'D6': 0.014545454545454545, 'D4': 0.012727272727272728, 'D9': 0.0036363636363636364, 'D7': 0.0036363636363636364})
-
-    sum_perct_d0_d1 = 0.81  # D0 + D1 (just as Shrikant originally indicated)
-
-    #
-
-    corpus = read_corpus("LocText", CORPUS_PERCENTANGE)
-
-    SENTENCE_SPLITTER.split(corpus)
-    TOKENIZER.tokenize(corpus)
-
-    print("# Documents", len(corpus), corpus.documents.keys())
-    (counter_nums, counter_percts) = corpus.compute_stats_relations_distances(REL_PRO_LOC_ID, ENTITY_MAP_FUN, RELATION_ACCEPT_FUN)
-    print("# Relations", sum(counter_nums.values()))
-    print("  ", counter_nums)
-    print("  ", counter_percts)
-
-    assert nums == counter_nums
-    assert math.isclose(sum_perct_d0_d1, (counter_percts['D0'] + counter_percts['D1']), abs_tol=0.01)
-
+    _test(
+        Entity.__repr__,
+        None,  # meaning: str.__eq__
+        #
+        0.81,
+        #
+        Counter({'D0': 351, 'D1': 95, 'D2': 53, 'D3': 23, 'D5': 9, 'D6': 8, 'D4': 7, 'D9': 2, 'D7': 2}),
+        Counter({'D0': 0.6381818181818182, 'D1': 0.17272727272727273, 'D2': 0.09636363636363636, 'D3': 0.04181818181818182, 'D5': 0.016363636363636365, 'D6': 0.014545454545454545, 'D4': 0.012727272727272728, 'D9': 0.0036363636363636364, 'D7': 0.0036363636363636364})
+    )
 
 def test_count_relations_dists_without_repetitions():
-    ENTITY_MAP_FUN = DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['lowercased']
-    RELATION_ACCEPT_FUN = None  # meaning: str.__eq__
-
-    # Documents 100
-    nums = Counter({'D0': 272, 'D1': 78, 'D2': 48, 'D3': 22, 'D5': 9, 'D4': 7, 'D6': 7, 'D9': 2, 'D7': 1})
-    nums_sum = sum(nums.values())
-    percts = Counter({'D0': 0.6098654708520179, 'D1': 0.17488789237668162, 'D2': 0.10762331838565023, 'D3': 0.04932735426008968, 'D5': 0.020179372197309416, 'D4': 0.01569506726457399, 'D6': 0.01569506726457399, 'D9': 0.004484304932735426, 'D7': 0.002242152466367713})
-
-    sum_perct_d0_d1 = 0.79  # D0 + D1
-
-    #
-
-    corpus = read_corpus("LocText", CORPUS_PERCENTANGE)
-
-    SENTENCE_SPLITTER.split(corpus)
-    TOKENIZER.tokenize(corpus)
-
-    print("# Documents", len(corpus), corpus.documents.keys())
-    (counter_nums, counter_percts) = corpus.compute_stats_relations_distances(REL_PRO_LOC_ID, ENTITY_MAP_FUN, RELATION_ACCEPT_FUN)
-    print("# Relations", sum(counter_nums.values()))
-    print("  ", counter_nums)
-    print("  ", counter_percts)
-
-    assert nums == counter_nums
-    assert math.isclose(sum_perct_d0_d1, (counter_percts['D0'] + counter_percts['D1']), abs_tol=0.01)
+    _test(
+        DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['lowercased'],
+        None,  # meaning: str.__eq__
+        #
+        0.80,
+        #
+        Counter({'D0': 278, 'D1': 78, 'D2': 44, 'D3': 22, 'D5': 9, 'D6': 7, 'D4': 5, 'D9': 2, 'D7': 1}),
+        Counter({'D0': 0.6233183856502242, 'D1': 0.17488789237668162, 'D2': 0.09865470852017937, 'D3': 0.04932735426008968, 'D5': 0.020179372197309416, 'D6': 0.01569506726457399, 'D4': 0.011210762331838564, 'D9': 0.004484304932735426, 'D7': 0.002242152466367713})
+    )
 
 
 def test_count_relations_dists_normalizations_without_repetitions():
-    ENTITY_MAP_FUN = DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['normalized_first']
-    RELATION_ACCEPT_FUN = None  # meaning: str.__eq__
-
-    # Documents 100
-
-    # Texts With Repetitions
-    nums = Counter({'D0': 210, 'D1': 52, 'D2': 32, 'D3': 15, 'D5': 9, 'D6': 6, 'D4': 5, 'D9': 2})
-    nums_sum = sum(nums.values())
-    percts = Counter({'D0': 0.6344410876132931, 'D1': 0.15709969788519637, 'D2': 0.09667673716012085, 'D3': 0.045317220543806644, 'D5': 0.027190332326283987, 'D6': 0.01812688821752266, 'D4': 0.015105740181268883, 'D9': 0.006042296072507553})
-
-    sum_perct_d0_d1 = 0.80  # D0 + D1
-
-    corpus = read_corpus("LocText", CORPUS_PERCENTANGE)
-
-    SENTENCE_SPLITTER.split(corpus)
-    TOKENIZER.tokenize(corpus)
-
-    print("# Documents", len(corpus), corpus.documents.keys())
-    (counter_nums, counter_percts) = corpus.compute_stats_relations_distances(REL_PRO_LOC_ID, ENTITY_MAP_FUN, RELATION_ACCEPT_FUN)
-    print("# Relations", sum(counter_nums.values()))
-    print("  ", counter_nums)
-    print("  ", counter_percts)
-
-    assert nums == counter_nums
-    assert math.isclose(sum_perct_d0_d1, (counter_percts['D0'] + counter_percts['D1']), abs_tol=0.01)
+    _test(
+        DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['normalized_first'],
+        None,  # meaning: str.__eq__
+        #
+        0.81,
+        #
+        Counter({'D0': 215, 'D1': 54, 'D2': 31, 'D3': 12, 'D5': 9, 'D6': 6, 'D9': 2, 'D4': 2}),
+        Counter({'D0': 0.649546827794562, 'D1': 0.16314199395770393, 'D2': 0.09365558912386707, 'D3': 0.03625377643504532, 'D5': 0.027190332326283987, 'D6': 0.01812688821752266, 'D9': 0.006042296072507553, 'D4': 0.006042296072507553})
+    )
 
 
 def test_count_relations_dists_normalizations_without_repetitions_considering_hierarchy():
+    _test(
+        DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['normalized_first'],
+        relation_accept_uniprot_go,
+        #
+        0.83,
+        #
+        Counter({'D0': 179, 'D1': 46, 'D2': 21, 'D3': 10, 'D5': 7, 'D6': 5, 'D9': 2, 'D4': 2}),
+        Counter({'D0': 0.6580882352941176, 'D1': 0.16911764705882354, 'D2': 0.07720588235294118, 'D3': 0.03676470588235294, 'D5': 0.025735294117647058, 'D6': 0.01838235294117647, 'D9': 0.007352941176470588, 'D4': 0.007352941176470588})
+    )
 
-    ENTITY_MAP_FUN = DocumentLevelRelationEvaluator.COMMON_ENTITY_MAP_FUNS['normalized_first']
-    RELATION_ACCEPT_FUN = relation_accept_uniprot_go
 
-    # Documents 100
+#
 
-    # Texts With Repetitions
-    nums = Counter({'D0': 176, 'D1': 44, 'D2': 22, 'D3': 12, 'D5': 7, 'D6': 5, 'D4': 4, 'D9': 2})
-    percts = Counter({'D0': 0.6470588235294118, 'D1': 0.16176470588235295, 'D2': 0.08088235294117647, 'D3': 0.04411764705882353, 'D5': 0.025735294117647058, 'D6': 0.01838235294117647, 'D4': 0.014705882352941176, 'D9': 0.007352941176470588})
 
-    sum_perct_d0_d1 = 0.81  # D0 + D1
-
+def _test(entity_map_fun, relation_accept_fun, expected_sum_perct_d0_d1, expected_nums, expected_percts):
     corpus = read_corpus("LocText", CORPUS_PERCENTANGE)
 
-    SENTENCE_SPLITTER.split(corpus)
-    TOKENIZER.tokenize(corpus)
+    # Note: the predictor will already split & tokenize the corpus. See the implementation for details
+    StubSamePartRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID).annotate(corpus)
 
-    print("# Documents", len(corpus), corpus.documents.keys())
-    (counter_nums, counter_percts) = corpus.compute_stats_relations_distances(REL_PRO_LOC_ID, ENTITY_MAP_FUN, RELATION_ACCEPT_FUN)
+    (counter_nums, counter_percts) = corpus.compute_stats_relations_distances(REL_PRO_LOC_ID, entity_map_fun, relation_accept_fun, predicted=True)
+
+    print()
+    print("# Documents", len(corpus))
     print("# Relations", sum(counter_nums.values()))
     print("  ", counter_nums)
     print("  ", counter_percts)
 
-    assert nums == counter_nums
-    assert math.isclose(sum_perct_d0_d1, (counter_percts['D0'] + counter_percts['D1']), abs_tol=0.01)
+    assert expected_nums == counter_nums
+    assert math.isclose(expected_sum_perct_d0_d1, (counter_percts['D0'] + counter_percts['D1']), abs_tol=0.01)
