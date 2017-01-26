2b18c17835d8c625332d1f5bbffbf6fbe1cea8a2
Juan Miguel Cejuela <i@juanmi.rocks>
2017-01-26 15:12:57 +0100

    do also kbest for feature selection

--- scripts/kbest_feature_selection.py
+++ scripts/kbest_feature_selection.py
@@ -1,10 +1,7 @@
 """
 ===================================================
-Recursive feature elimination with cross-validation
+KBest feature Selection
 ===================================================
-
-A recursive feature elimination example with automatic tuning of the
-number of features selected with cross-validation.
 """
 print(__doc__)
 
@@ -13,6 +10,9 @@ from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
 from sklearn.feature_selection import RFECV
 from sklearn.datasets import make_classification
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import mutual_info_classif
 
 from nalaf.learning.lib.sklsvm import SklSVM
 from nalaf.structures.data import Dataset
@@ -27,29 +27,20 @@ locTextModel = LocTextSSmodelRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID, p
 locTextModel.pipeline.execute(corpus, train=True)
 X, y = locTextModel.model.write_vector_instances(corpus, locTextModel.pipeline.feature_set)
 
-scoring='f1_macro'
-
-rfecv = RFECV(
-    verbose=1,
-    n_jobs=-1,
-    estimator=locTextModel.model.model,
-    step=1,
-    cv=my_cv_generator(len(y)),
-    scoring=scoring
-)
+kbest = SelectKBest(mutual_info_classif, k=600)
 
 start = time.time()
-rfecv.fit(X, y)
+X_new = kbest.fit_transform(X, y)
 end = time.time()
 
 print("TIME for feature selection: ", (end - start))
 
-print("Optimal number of features : %d" % rfecv.n_features_)
+print("Optimal number of features: ", X_new.shape[1])
 
 selected = []
 nonselected = []
 
-for index, value in enumerate(rfecv.support_):
+for index, value in enumerate(kbest.get_support()):
     if value:
         selected.append(index)
     else:
@@ -58,11 +49,3 @@ for index, value in enumerate(rfecv.support_):
 # print("NON Selected features", nonselected)
 print()
 print("Selected features", selected)
-print("Max performance for {}: {}".format(scoring, rfecv.grid_scores_[rfecv.n_features_ -1]))
-
-# Plot number of features VS. cross-validation scores
-plt.figure()
-plt.xlabel("Number of features selected")
-plt.ylabel("Cross validation score (nb of correct classifications)")
-plt.plot(range(1, len(rfecv.grid_scores_) + 1), rfecv.grid_scores_)
-plt.show()
