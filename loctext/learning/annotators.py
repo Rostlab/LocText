90627def4832fe6a2d8b9adb18008f50ef00005e
shpendm <shpendmahmuti@gmail.com>
2017-01-29 23:48:49 +0100

    #8 is working and tests are being performed

--- loctext/learning/annotators.py
+++ loctext/learning/annotators.py
@@ -2,6 +2,8 @@ from nalaf.learning.taggers import RelationExtractor
 from nalaf.learning.taggers import StubSameSentenceRelationExtractor
 from nalaf.learning.svmlight import SVMLightTreeKernels
 from nalaf.structures.relation_pipelines import RelationExtractionPipeline
+from pyatspi import document
+
 from loctext.features.specific import LocationWordFeatureGenerator
 from loctext.features.specific import ProteinWordFeatureGenerator
 from nalaf.features.relations import TokenFeatureGenerator
@@ -9,8 +11,10 @@ from nalaf.features.relations.context import LinearDistanceFeatureGenerator
 from nalaf.features.relations.context import EntityOrderFeatureGenerator
 from nalaf.features.relations.context import IntermediateTokensFeatureGenerator
 from nalaf.features.relations.path import PathFeatureGenerator
-from nalaf.features.relations.sentence import NamedEntityCountFeatureGenerator, BagOfWordsFeatureGenerator, StemmedBagOfWordsFeatureGenerator
-from nalaf.features.relations.entityhead import EntityHeadTokenUpperCaseFeatureGenerator, EntityHeadTokenDigitsFeatureGenerator, EntityHeadTokenPunctuationFeatureGenerator
+from nalaf.features.relations.sentence import NamedEntityCountFeatureGenerator, BagOfWordsFeatureGenerator, \
+    StemmedBagOfWordsFeatureGenerator
+from nalaf.features.relations.entityhead import EntityHeadTokenUpperCaseFeatureGenerator, \
+    EntityHeadTokenDigitsFeatureGenerator, EntityHeadTokenPunctuationFeatureGenerator
 from nalaf.preprocessing.edges import SimpleEdgeGenerator, SentenceDistanceEdgeGenerator
 from nalaf.utils.readers import StringReader
 from nalaf import print_verbose, print_debug
@@ -23,7 +27,6 @@ import urllib.request
 
 
 class LocTextSSmodelRelationExtractor(RelationExtractor):
-
     def __init__(
             self,
             entity1_class,
@@ -45,15 +48,17 @@ class LocTextSSmodelRelationExtractor(RelationExtractor):
             feature_generators = self.feature_generators()
 
         edge_generator = SentenceDistanceEdgeGenerator(entity1_class, entity2_class, rel_type, distance=0)
-        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type, edge_generator=edge_generator, feature_generators=feature_generators)
+        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type,
+                                                                             edge_generator=edge_generator,
+                                                                             feature_generators=feature_generators)
 
-        assert feature_generators == self.pipeline.feature_generators or feature_generators == [], str((feature_generators, self.pipeline.feature_generators))
+        assert feature_generators == self.pipeline.feature_generators or feature_generators == [], str(
+            (feature_generators, self.pipeline.feature_generators))
 
         self.execute_pipeline = execute_pipeline
 
         # TODO this would require setting the default model_path
         self.svmlight = svmlight if svmlight else SVMLightTreeKernels(**svmlight_params)
-
 
     def annotate(self, target_corpus):
         if self.execute_pipeline:
@@ -65,10 +70,8 @@ class LocTextSSmodelRelationExtractor(RelationExtractor):
 
         return target_corpus
 
-
     def feature_generators(self):
         return __class__.default_feature_generators(self.entity1_class, self.entity2_class)
-
 
     @staticmethod
     def default_feature_generators(prot_e_id, loc_e_id, graphs=None):
@@ -196,7 +199,6 @@ class LocTextSSmodelRelationExtractor(RelationExtractor):
 
 
 class LocTextDSmodelRelationExtractor(RelationExtractor):
-
     def __init__(
             self,
             entity1_class,
@@ -218,15 +220,17 @@ class LocTextDSmodelRelationExtractor(RelationExtractor):
             feature_generators = self.feature_generators()
 
         edge_generator = SentenceDistanceEdgeGenerator(entity1_class, entity2_class, rel_type, distance=1)
-        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type, edge_generator=edge_generator, feature_generators=feature_generators)
+        self.pipeline = pipeline if pipeline else RelationExtractionPipeline(entity1_class, entity2_class, rel_type,
+                                                                             edge_generator=edge_generator,
+                                                                             feature_generators=feature_generators)
 
-        assert feature_generators == self.pipeline.feature_generators or feature_generators == [], str((feature_generators, self.pipeline.feature_generators))
+        assert feature_generators == self.pipeline.feature_generators or feature_generators == [], str(
+            (feature_generators, self.pipeline.feature_generators))
 
         self.execute_pipeline = execute_pipeline
 
         # TODO this would require setting the default model_path
         self.svmlight = svmlight if svmlight else SVMLightTreeKernels(**svmlight_params)
-
 
     def annotate(self, target_corpus):
         if self.execute_pipeline:
@@ -238,10 +242,8 @@ class LocTextDSmodelRelationExtractor(RelationExtractor):
 
         return target_corpus
 
-
     def feature_generators(self):
         return __class__.default_feature_generators(self.entity1_class, self.entity2_class)
-
 
     @staticmethod
     def default_feature_generators(prot_e_id, loc_e_id, graphs=None):
@@ -252,7 +254,6 @@ class LocTextDSmodelRelationExtractor(RelationExtractor):
 
 
 class LocTextCombinedModelRelationExtractor(RelationExtractor):
-
     def __init__(
             self,
             entity1_class,
@@ -260,16 +261,13 @@ class LocTextCombinedModelRelationExtractor(RelationExtractor):
             rel_type,
             ss_model,
             ds_model):
-
         super().__init__(entity1_class, entity2_class, rel_type)
 
         self.ss_model = ss_model
         self.ds_model = ds_model
         self.submodels = [self.ss_model, self.ds_model]
 
-
     def annotate(self, target_corpus):
-
         for model in self.submodels:
             model.annotate(target_corpus)
 
@@ -277,35 +275,30 @@ class LocTextCombinedModelRelationExtractor(RelationExtractor):
 
 
 class StringTagger(Tagger):
-    def __init__(self):
+    def __init__(self, send_whole_once):
+        self.send_whole_once = send_whole_once
         super().__init__([UNIPROT_NORM_ID, STRING_NORM_ID])
 
-    # def startServer(self):
-    #     os.popen("docker build -t tagg .")
-    #     os.popen("docker run -p 5000:5000 tagg:1.0")
-
-    # Gets String Tagger JSON response, by making a REST call.
+    # gets String Tagger JSON response, by making a REST call.
     def get_string_tagger_json_response(self, payload):
-        #self.startServer()
         base_url = "http://127.0.0.1:5000/annotate/post"
         try:
             json_response = requests.post(base_url, json=dict(text=payload))
             json_response.status_code = 200
             response_data = json_response.json()
         except requests.exceptions.ConnectionError as err:
-            #Maybe set up for a retry, or continue in a retry loop
-            print("The docker STRING tagger REST server cannot be reached. \n"
-                  "For installation and running the server, please refer to https://github.com/juanmirocks/STRING-tagger-server/tree/feature/Testing")
-            #sys.exit(1)
+            # maybe set up for a retry, or continue in a retry loop
+            print(
+                "Sever is not running. For this application you need to install Docker https://docs.docker.com/engine/installation/ \n"
+                "You only need to build the docker image once, like this: '$docker build -t tagger .' \n"
+                "To run the docker image, you type this command: '$docker run -p 5000:5000 tagger'")
         return response_data
 
-
-    # Return true if server is running (for testing purposes)
+    # return true if server is running (for testing purposes)
     def server_is_running(host, url):
         return urllib.request.urlopen(url).getcode() == 200
 
-
-    # Sets the predicted annotations of the parts based on JSON response entity values.
+    # sets the predicted annotations of the parts based on JSON response entity values.
     def set_predicted_annotations(self, json_response, part):
 
         entities = json_response["entities"]
@@ -323,22 +316,14 @@ class StringTagger(Tagger):
                 if str(norm["type"]).isdigit():
                     if counter > 1:
                         string_id += ","
-                    string_id += "string:"+str(norm["id"])
+                    string_id += str(norm["id"])
                 else:
                     if counter > 1:
                         uniprot_id += ","
-                    uniprot_id += str(norm["id"])
-
-                # Right now we do not have string_id like "string:9606" with string value in it,
-                # hence below code is commented
-                # if norm["type"].startswith("uniprot"):
-                #     if counter > 1:
-                #         uniprot_id += ","
-                #     uniprot_id = str(norm["id"])
-                # else:
-                #     if counter > 1:
-                #         string_id += ","
-                #     string_id = str(norm["id"])
+                    if len(str(norm["id"]).split('|')):
+                        uniprot_id += str(norm["id"]).split('|')[0]
+                    else:
+                        uniprot_id += str(norm["id"])
 
                 counter += 1
 
@@ -352,17 +337,69 @@ class StringTagger(Tagger):
 
             part.predicted_annotations.append(entity_dictionary)
 
-    # Primary method which will be called to set predicated annotations based on JSON response from STRING tagger.
+    # sets the predicted annotations of the whole text based on JSON response entity values
+    def set_entities_of_whole_doc(self, json_response, document):
+
+        entities = json_response["entities"]
+
+        for entity in entities:
+            start = entity["start"]
+            end = entity["end"]
+            normalizations = entity["normalizations"]
+            uniprot_id = ""
+            string_id = ""
+            counter = 0
+            length = 1
+            for partId, part in document.parts.items():
+                text = part.text[start - length:end - length + 1]
+
+                if text != "":
+                    for norm in normalizations:
+
+                        if str(norm["type"]).isdigit():
+                            if counter > 1:
+                                string_id += ","
+                            string_id += str(norm["id"])
+                        else:
+                            if counter > 1:
+                                uniprot_id += ","
+                            if len(str(norm["id"]).split('|')):
+                                uniprot_id += str(norm["id"]).split('|')[0]
+                            else:
+                                uniprot_id += str(norm["id"])
+
+                        counter += 1
+
+                    if counter > 1:
+                        uniprot_id = uniprot_id[:len(uniprot_id)]
+                        string_id = string_id[:len(string_id)]
+
+                    norm_dictionary = {UNIPROT_NORM_ID: uniprot_id, STRING_NORM_ID: string_id}
+
+                    entity_dictionary = Entity(class_id=UNIPROT_NORM_ID, offset=start - length, text=text,
+                                               norm=norm_dictionary)
+
+                    part.predicted_annotations.append(entity_dictionary)
+
+                    break
+                length += len(part.text) + 1
+
+    # primary method which will be called to set predicated annotations based on JSON response from STRING tagger.
     def annotate(self, dataset):
-        # Verify entity offsets - No warnings should be displayed
-        dataset.validate_entity_offsets()
 
-        for part in dataset.parts():
-            # Retrieve JSON response
-            json_response = self.get_string_tagger_json_response(part.text)
+        for docId, document in dataset.documents.items():
 
-            # Set entity information to part.predicated_annotations list
-            self.set_predicted_annotations(json_response, part)
+            if self.send_whole_once:
+                # Note: dataset contains only the text content without separating into parts.
+                json_response = self.get_string_tagger_json_response(document.get_text())
+                self.set_entities_of_whole_doc(json_response, document)
+            else:
+                for partId, part in document.parts.items():
+                    # Retrieve JSON response
+                    json_response = self.get_string_tagger_json_response(part.text)
 
-        # Verify entity offsets - No warnings should be displayed
-        dataset.validate_entity_offsets()
+                    # Set entity information to part.predicated_annotations list
+                    self.set_predicted_annotations(json_response, part)
+
+            # Verify entity offsets - No warnings should be displayed
+            dataset.validate_entity_offsets()
--- tests/test_String_tagger.py
+++ tests/test_String_tagger.py
@@ -1,36 +1,44 @@
-from nalaf.utils.readers import StringReader
 from loctext.learning.annotators import StringTagger
-import os
-
-def get_string_tagger_post_request_response():
-
-    #content = "Arginine metabolism in Saccharomyces cerevisiae: subcellular localization of the enzymes.\nSubcellular localization of enzymes of arginine metabolism in Sacchar"
-    filename = 'stringTaggerData.txt'
-    path = os.path.normpath(os.getcwd() + os.sep + os.pardir) + "/resources/corpora/LocText/LocText_anndoc_original_without_normalizations/LocText_plain_html/pool/a.cttjDbKt3vxJIqCWvfhuhVjjfy-15486337.plain.html"
-    path_to_file = os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/resources/corpora/LocText/'+filename
-    with open(path_to_file, 'r') as content_file:
-        content = content_file.read()
-
-    dataset = StringReader(content).read()
-    #dataset = StringReader(string_tagger_request_text).read()
-
-    # Verify entity offsets - No warnings should be displayed
-    dataset.validate_entity_offsets()
-
-    return dataset
+from loctext.learning.train import read_corpus
 
 
-def annotate_string_tagger():
-    dataset= get_string_tagger_post_request_response()
-    #StringTagger().server_is_running()
-    StringTagger().annotate(dataset)
+# test when content of documents is sent in parts
+def test_annotate_string_tagger_whole_text_false():
+    dataset = read_corpus("LocText", corpus_percentage=0.04)  # 10116 Rattus norvegicus (rat)
+    StringTagger(False).annotate(dataset)
 
-#assert statements needs to be written here for verification based on test. Many tests are already performed.
+
+# test when the whole content of document is sent at once
+def test_annotate_string_tagger_whole_text_true():
+    dataset = read_corpus("LocText", corpus_percentage=0.04)
+    StringTagger(True).annotate(dataset)
+
+
+# get the number of annotated entities in a dataset
+def num_annotated_entities(corpus):
+    return len(list(corpus.predicted_entities()))
+
+
+# test if number of annotations of parts is less than or equal to the number of annotations of the whole content
+def test_number_of_tagged_entities():
+    dataset_false = read_corpus("LocText", corpus_percentage=0.04)
+    dataset_true = read_corpus("LocText", corpus_percentage=0.04)
+    StringTagger(False).annotate(dataset_false)
+    StringTagger(True).annotate(dataset_true)
+    assert num_annotated_entities(dataset_false) <= num_annotated_entities(dataset_true)
+
+
+# assert statements needs to be written here for verification based on test.
+# Many tests are already performed in the String-Tagger.
 def test_json_response():
-    assert StringTagger().get_string_tagger_json_response("simple text") == {"entities":[]}
-    assert StringTagger().get_string_tagger_json_response("p53") == {"entities":[{"end": 4,"normalizations":[{"id":"ENSP00000269305","type":9606},{"id":"P04637|P53_HUMAN","type":"uniprot:9606"}],"start":1}]}
-    assert StringTagger().get_string_tagger_json_response("p53") != {"entities":[]}
+    assert StringTagger(False).get_string_tagger_json_response("simple text") == {"entities": []}
+    assert StringTagger(False).get_string_tagger_json_response("p53") == {"entities": [{"end": 3, "normalizations": [
+        {"id": "ENSP00000269305", "type": 9606}, {"id": "P04637|P53_HUMAN", "type": "uniprot:9606"}], "start": 1}]}
+    assert StringTagger(False).get_string_tagger_json_response("p53") != {"entities": []}
+    assert StringTagger(True).get_string_tagger_json_response("p53") == {"entities": [{"end": 3, "normalizations": [
+        {"id": "ENSP00000269305", "type": 9606}, {"id": "P04637|P53_HUMAN", "type": "uniprot:9606"}], "start": 1}]}
+
 
 # passes if server is running
-def check_if_server_is_running():
-    assert StringTagger().server_is_running("http://localhost:5000/")== True
+def test_check_if_server_is_running():
+    assert StringTagger(False).server_is_running("http://localhost:5000/") == True
