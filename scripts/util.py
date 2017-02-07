20394dc47cddc2e8de54e1c4556309b47357d38b
Juan Miguel Cejuela <i@juanmi.rocks>
2017-02-07 17:56:45 +0100

    do feature selection of any DX

--- scripts/util.py
+++ scripts/util.py
@@ -41,10 +41,10 @@ def my_cv_generator(groups, num_instances=None):
         yield tr, ev
 
 
-def get_model_and_data():
+def get_model_and_data(sentence_distance=0):
     corpus = read_corpus("LocText")
     # TODO the specific parameters like C=1 or even `linear` are controversial -- Maybe I should I change that
-    annotator = LocTextDXModelRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID, preprocess=True, kernel='linear', C=1)
+    annotator = LocTextDXModelRelationExtractor(PRO_ID, LOC_ID, REL_PRO_LOC_ID, sentence_distance, preprocess=True, kernel='linear', C=1)
     annotator.pipeline.execute(corpus, train=True)
     X, y, groups = annotator.model.write_vector_instances(corpus, annotator.pipeline.feature_set)
 
