a726e4461818592b279428cb1376b580ee37672f
Juan Miguel Cejuela <i@juanmi.rocks>
2017-02-11 12:19:35 +0100

    remove unnecessary and add another special case

--- tests/test_String_tagger.py
+++ tests/test_String_tagger.py
@@ -11,24 +11,6 @@ TAGGER_SEND_EGAL = TAGGER_SEND_PARTS
 run_tests = TAGGER_SEND_PARTS.is_server_running("http://localhost:5000/")
 
 
-# test when content of documents is sent in parts
-def test_annotate_string_tagger_whole_text_false():
-    if run_tests:
-        dataset = read_corpus("LocText", corpus_percentage=1.0)
-        TAGGER_SEND_PARTS.annotate(dataset)
-
-        # Special analysis as it contains unicode chracters (μ)
-        for e in dataset.documents["23543752"].predicted_entities():
-            print(e)
-
-
-# test when the whole content of document is sent at once
-def test_annotate_string_tagger_whole_text_true():
-    if run_tests:
-        dataset = read_corpus("LocText", corpus_percentage=1.0)
-        TAGGER_SEND_WHOLE.annotate(dataset)
-
-
 # get the number of annotated entities in a dataset
 def num_predicted_entities(corpus):
     return len(list(corpus.predicted_entities()))
@@ -43,13 +25,20 @@ def test_number_of_tagged_entities():
 
         TAGGER_SEND_PARTS.annotate(dataset_parts)
         TAGGER_SEND_WHOLE.annotate(dataset_whole)
-        # with all organisms explicitly added, parts currently gives one more entity.
-        # with only human explicitly added, then the inequality holds
+
+        for docid, document in dataset_parts.documents.items():
+            if docid in [
+                "23543752",  # Special analysis as it contains unicode chracters (μ)
+                "23150645"  # also unicode and annotation of reductase where reductase as string alone is not tagged
+            ]:
+                for e in document.predicted_entities():
+                    print(e)
+                print()
 
         num_preds_with_parts = num_predicted_entities(dataset_parts)
         num_preds_with_whole = num_predicted_entities(dataset_whole)
 
-        print("Numbers, real: ", len(list(dataset_parts.entities())), "vs. pred", num_preds_with_parts, num_preds_with_whole)
+        print("Numbers, real:", len(list(dataset_parts.entities())), "vs. pred: ", num_preds_with_parts, num_preds_with_whole)
 
         assert num_preds_with_parts == num_preds_with_whole
 
