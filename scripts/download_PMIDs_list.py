ceb7e328fd9fccebd4391971b2d4e2550fa80dc1
Juan Miguel Cejuela <i@juanmi.rocks>
2017-02-11 19:33:09 +0100

    add support to download and cache! real PMIDs

--- scripts/download_PMIDs_list.py
+++ scripts/download_PMIDs_list.py
@@ -1,14 +1,25 @@
+import sys
 import urllib.request
+
+from nalaf.utils.download import DownloadArticle
+
+call_online_string_tagger = False
+
+##
 
 in_path = "resources/features/human_localization_all_PMIDs_only__2016-11-20.tsv"
 
-with open(in_path) as f:
-    for pmid in f:
-        pmid = pmid.strip()
+with DownloadArticle() as PMID_DL:
 
-        url = "http://compartments.jensenlab.org/document/{}/annotations".format(pmid)
-        out_path = "resources/features/human_localization_all_PMIDs_only_StringTagger_results__2016-11-20/{}.json".format(pmid)
+    with open(in_path) as f:
+        for pmid in f:
+            pmid = pmid.strip()
 
-        print(url)
+            for dl_pmid, doc in PMID_DL.download([pmid]):
+                print(dl_pmid)
 
-        urllib.request.urlretrieve(url, out_path)
+            if call_online_string_tagger:
+                url = "http://compartments.jensenlab.org/document/{}/annotations".format(pmid)
+                out_path = "resources/features/human_localization_all_PMIDs_only_StringTagger_results__2016-11-20/{}.json".format(pmid)
+                print("\t", url)
+                urllib.request.urlretrieve(url, out_path)
