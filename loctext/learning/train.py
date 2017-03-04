3d1edc85468994da8e98b3f776ba4f31e0ee45e1
Juan Miguel Cejuela <i@juanmi.rocks>
2017-03-04 12:13:15 +0100

    minor

--- loctext/learning/train.py
+++ loctext/learning/train.py
@@ -406,8 +406,7 @@ def read_corpus(corpus_name, corpus_percentage=1.0, predict_entities=False, retu
         return corpus
 
 
-def get_evaluator(evaluation_level, evaluate_only_on_edges_plausible_relations):
-    normalization_penalization = "soft"
+def get_evaluator(evaluation_level, evaluate_only_on_edges_plausible_relations=False, normalization_penalization="hard"):
 
     if evaluation_level == 1:
         ENTITY_MAP_FUN = Entity.__repr__
