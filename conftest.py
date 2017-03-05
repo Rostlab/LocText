47a8550e8a005ea487db769e1060de910103d205
Juan Miguel Cejuela <i@juanmi.rocks>
2017-03-05 09:43:18 +0100

    fix

--- conftest.py
+++ conftest.py
@@ -3,15 +3,12 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption('--corpus_percentage', type=float, default=1.0, help='e.g. 1 == full corpus; 0.5 == 50% of corpus')
+    parser.addoption('--evaluation_level', type=int)
 
 
 @pytest.fixture
 def corpus_percentage(request):
     return request.config.getoption("--corpus_percentage")
-
-
-def pytest_addoption(parser):
-    parser.addoption('--evaluation_level', type=int)
 
 
 @pytest.fixture
