16bba689ebf0dae2984d02933afadc2dc38034a8
Juan Miguel Cejuela <i@juanmi.rocks>
2017-04-16 14:18:20 +0200

    add TODO

--- loctext/learning/train.py
+++ loctext/learning/train.py
@@ -437,6 +437,7 @@ def read_corpus(corpus_name, corpus_percentage=1.0, predict_entities=None, retur
         return corpus
 
 
+# TODO BUG
 def get_evaluator(evaluation_level, evaluate_only_on_edges_plausible_relations=False, normalization_penalization="no"):
 
     if evaluation_level == 1:
